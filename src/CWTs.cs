18497788a32856d0629c9a4d2ae003e4f5177e13
alduris <152772325+alduris@users.noreply.github.com>
2024-06-30 00:29:32 -0400

    More setup

---
+++ src/CWTs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using SlugTemplate;
+
+namespace Dreamer
+{
+    internal static class CWTs
+    {
+        private static readonly ConditionalWeakTable<Player, DreamerData> playerCWT = new();
+        public static bool TryGetData(Player player, out DreamerData data)
+        {
+            if (player.SlugCatClass == Plugin.Dreamer)
+            {
+                data = playerCWT.GetValue(player, _ => new DreamerData());
+                return true;
+            }
+            data = null;
+            return false;
+        }
+    }
+
+    internal class DreamerData
+    {
+        public bool astral = false;
+    }
+}
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BepInEx;
+using Dreamer;
 using UnityEngine;
 
 namespace SlugTemplate
@@ -8,10 +9,21 @@ namespace SlugTemplate
     class Plugin : BaseUnityPlugin
     {
         private const string MOD_ID = "alduris.dreamer";
+        public static readonly SlugcatStats.Name Dreamer = new("Dreamer", false);
 
         // Add hooks
         public void OnEnable()
         {
+            On.Player.Update += Player_Update;
+        }
+
+        private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+        {
+            orig(self, eu);
+            if (CWTs.TryGetData(self, out var data))
+            {
+                //
+            }
         }
     }
 }
